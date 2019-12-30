3632f82f11bdacaa341e8b64fe998a9c50bd55e0
HTMLProgrammer2001 <cssuperpy@gmail.com>
2019-12-31 00:55:02 +0200

    Team and portfolio

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 $(document).ready(
 	function(){
+		//accordion
 		let active_slide = 0,
 			slides = $('#accordion > .desc-serv-slide');
 
 		$('#accordion .desc-serv-slide-header').on('click', function(e){
 			if(active_slide != -1 && active_slide != slides.index($(this).closest('.desc-serv-slide'))){
-				$(slides[active_slide]).find('.toggle').html('&or;');
+				$(slides[active_slide]).find('.toggle').removeClass('fa-angle-up').addClass('fa-angle-down');
 				$(slides[active_slide]).find('.desc-serv-slide-content').animate({
 					height: 0
 				}, 500);
@@ -13,11 +14,21 @@ $(document).ready(
 
 			if(active_slide != slides.index($(this).closest('.desc-serv-slide'))){
 				active_slide = slides.index($(this).closest('.desc-serv-slide'));
-				$(slides[active_slide]).find('.toggle').html('&and;');
+				$(slides[active_slide]).find('.toggle').removeClass('fa-angle-down').addClass('fa-angle-up');
 				$(slides[active_slide]).find('.desc-serv-slide-content').animate({
 					height: 140
 				}, 500);
 			}
+		});
+
+		//sliders
+		$('.recension-slider').slick({
+			dots: false,
+			adaptiveHeight: true,
+			arrows: true,
+			prevArrow: '<div class = "prev-arrow"><i class = "fa fa-angle-left"></i></div>',
+			nextArrow: '<div class = "next-arrow"><i class = "fa fa-angle-right"></i></div>',
+			cssEasy: 'linear'
 		});
 	}
 );
